90c67905e8f2db8767fc2c9c4f6c8ac2ee465178
affaks <detbeta@gmail.com>
2024-10-04 14:29:33 +0200

    check 1

--- main.py
+++ main.py
@@ -33,11 +33,6 @@ def main():
         for obj in updatable:
             obj.update(dt)
 
-        for asteroid in asteroids:
-            if asteroid.collides_with(player):
-                print("Game over!")
-                sys.exit()
-                
         screen.fill("black")
 
         for obj in drawable:
