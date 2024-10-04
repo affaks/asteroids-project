7267ab4e4afd0a9c743311df0195918ab734bf0a
affaks <detbeta@gmail.com>
2024-10-04 11:30:41 +0200

    delete all the #

--- main.py
+++ main.py
@@ -2,23 +2,15 @@ import pygame
 from constants import *
 
 def main():
-    # Initialize pygame
     pygame.init()
-
-    # Create a window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # Game loop
     while True:
-        # Check for user events (like closing the window)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return  # Exit the game loop
+                return
 
-        # Fill the screen with black
         screen.fill((0, 0, 0))
-
-        # Refresh the display
         pygame.display.flip()
 
 if __name__ == "__main__":
